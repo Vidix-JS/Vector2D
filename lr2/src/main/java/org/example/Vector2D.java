07b9a01eba6b0ec471d8963b482cbf5cd29e33a5
Vidix-JS <vadim.bakulin.1996@mail.ru>
2024-12-05 22:20:46 +0300

    Добавление конструтора по умолчанию, конструктора при двух параметрах, конструтора копирования и метод для вывода вектора и использование их в методе main

--- lr2/src/main/java/org/example/Vector2D.java
+++ lr2/src/main/java/org/example/Vector2D.java
@@ -1,5 +1,29 @@
 package org.example;
 
-public class Vector2D {
+import java.util.Locale;
 
+public class Vector2D {
+    private double vX; // Значение проекции вектора оси
+    private double vY; // Значение проекции вектора оси
+
+    public Vector2D(){ // конструктор без параметров
+        this.vX = 1;
+        this.vY = 1;
+    }
+
+    public Vector2D(double vX, double vY){
+        this.vX = vX;
+        this.vY = vY;
+    }
+    public Vector2D(Vector2D v){ // конструктор копирования
+        this.vX = v.vX;
+        this.vY = v.vY;
+    }
+
+    public void print(){ // метод для вывода
+        System.out.print("(");
+        System.out.print(String.format(Locale.US, "%.2f", vX));
+        System.out.print(", ");
+        System.out.print(String.format(Locale.US, "%.2f", vY) + ")\n");
+    }
 }
